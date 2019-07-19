e45caf03f68a1fb518e30c9009b5c49941078a53
Sachin Wani <sachinravindra.wani@mavs.uta.edu>
2019-07-18 19:09:41 -0500

    initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { LoginComponent } from './auth/login/login.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    LoginComponent,
+    AppRoutingModule
   ],
   imports: [
     BrowserModule
