2352e560e6b8fb96d47731126ea88e6beeea6350
Sachin Wani <sachinravindra.wani@mavs.uta.edu>
2019-07-19 18:28:15 -0500

    NavigationCommit
    Added navigation, toolbar, sidenav

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,16 +1,29 @@
 import { NgModule} from '@angular/core';
-import { MatFormFieldModule, MatButtonModule, MatInputModule, MatCheckboxModule } from '@angular/material';
+import { MatFormFieldModule,
+         MatButtonModule,
+         MatInputModule,
+         MatCheckboxModule,
+         MatSidenavModule,
+         MatToolbarModule,
+         MatIconModule
+        } from '@angular/material';
 
 @NgModule({
     imports : [MatFormFieldModule,
                MatButtonModule,
                MatInputModule,
-               MatCheckboxModule
+               MatCheckboxModule,
+               MatSidenavModule,
+               MatToolbarModule,
+               MatIconModule
     ],
     exports : [MatFormFieldModule,
                MatButtonModule,
                MatInputModule,
-               MatCheckboxModule
+               MatCheckboxModule,
+               MatSidenavModule,
+               MatToolbarModule,
+               MatIconModule
     ]
 })
 export class MaterialModule {
